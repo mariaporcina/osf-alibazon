4f21e5e89be675f326a98eac1a2e90885916af72
maria-osfdigital <maria.porcina@osf.digital>
2022-09-13 17:45:36 -0300

    setting up subsubcategory route

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ let express = require('express');
 let router = express.Router();
 let axios = require('axios');
 
-const BASE_URL = 'https://backend-academy-osf.herokuapp.com/api/';
+const BASE_URL = 'https://backend-academy-osf.herokuapp.com/api';
 const SECRET_KEY = '$2a$08$S7ywFQvTnICkPEJfEhSBIuc5lVn9MRBWuO6esbw810490In27vky.'
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const response = await axios.get(`${BASE_URL}categories?secretKey=${SECRET_KEY}`)
+  const response = await axios.get(`${BASE_URL}/categories?secretKey=${SECRET_KEY}`)
 
   const categories = response.data;
   const rootCategories = categories.filter( ({ parent_category_id }) => parent_category_id === 'root');
