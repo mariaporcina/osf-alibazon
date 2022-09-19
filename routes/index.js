2771f7c973d039f95c88369d51e4a119d7bfd643
maria-osfdigital <maria.porcina@osf.digital>
2022-09-19 16:27:16 -0300

    updating root page endpoint

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,9 @@ const SECRET_KEY = '$2a$08$S7ywFQvTnICkPEJfEhSBIuc5lVn9MRBWuO6esbw810490In27vky.
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    const response = await axios.get(`${BASE_URL}/categories?secretKey=${SECRET_KEY}`)
-
-    const categories = response.data;
-    const rootCategories = categories.filter( ({ parent_category_id }) => parent_category_id === 'root');
+    const rootCategories = await axios.get(`${BASE_URL}/categories/parent/root?secretKey=${SECRET_KEY}`)
+    .then(res => res.data)
+    .catch(err => console.error(err));
 
     res.render('index', {
         title: 'Alibazon',
